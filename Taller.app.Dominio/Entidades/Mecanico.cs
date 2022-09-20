b09ca760002c2a36bf1b2cdc77c09a079d514e48
joseat23 <josed-2311@hotmail.com>
2022-09-20 16:58:26 -0500

    first commit

---
+++ Taller.app.Dominio/Entidades/Mecanico.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Taller.app.Dominio.Entidades
+
+{
+    public class Mecanico: Persona
+    {
+        public string nivelEstudio {get; set;}
+
+    }
+
+
+
+}
---
+++ Taller.app.Dominio/Entidades/Persona.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taller.app.Dominio.Entidades
+{
+    public class Persona
+    {
+        public string id {get;set;}
+        public string nombres {get;set;}
+        public string apellidos {get;set;}
+        public string telefono {get;set;}
+        public string fechaNacimiento {get;set;}
+        public string correo {get;set;}
+        public string contraseña {get;set;}
+
+    }
+}
